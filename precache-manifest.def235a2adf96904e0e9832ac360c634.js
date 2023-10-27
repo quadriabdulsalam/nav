0f5ca9b8c102222b64773a935c4c4c5d60b40313
dimeji54 <oladimejiabdulsalam54@gmail.com>
2023-10-27 10:41:34 -0400

    Updates

---
+++ precache-manifest.def235a2adf96904e0e9832ac360c634.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1dc16de3c63e41cb6b7",
+    "url": "/nav/static/css/main.c5fc5886.chunk.css"
+  },
+  {
+    "revision": "f1dc16de3c63e41cb6b7",
+    "url": "/nav/static/js/main.f1dc16de.chunk.js"
+  },
+  {
+    "revision": "3f5a8f4c7bc0532f173e",
+    "url": "/nav/static/js/1.3f5a8f4c.chunk.js"
+  },
+  {
+    "revision": "91bbac464fe9c0dedb41",
+    "url": "/nav/static/js/2.91bbac46.chunk.js"
+  },
+  {
+    "revision": "cabe56a3f47bbb84d9e4",
+    "url": "/nav/static/js/runtime~main.cabe56a3.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/nav/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "cd1df1d77a991d674adb461f3de76cdb",
+    "url": "/nav/index.html"
+  }
+];
